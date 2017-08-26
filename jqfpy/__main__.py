df1a244adf81aac34311499f81dddd53d8d6a3c1
podhmo <ababjam61+github@gmail.com>
2017-08-27 07:41:54 +0900

    with --slurp, non buffering output

--- jqfpy/__main__.py
+++ jqfpy/__main__.py
@@ -33,9 +33,10 @@ def main():
     args = parser.parse_args()
     rvalname = jqfpy.create_rvalname()
     pycode = jqfpy.create_pycode(args.code, rvalname)
+    fp = sys.stdout
 
     if args.show_code_only:
-        _describe_pycode(pycode, fp=sys.stdout, indent="")
+        _describe_pycode(pycode, fp=fp, indent="")
         sys.exit(0)
 
     if is_fd_alive(args.input):
@@ -47,32 +48,33 @@ def main():
         sys.exit(0)
 
     for stream in files:
-        d = loader.load(stream, slurp=args.slurp_input)
-        try:
-            r = jqfpy.exec_pycode(d, pycode, rvalname)
-        except Exception as e:
-            fp = sys.stderr
-            print("\x1b[32m\x1b[1mcode:\x1b[0m", file=fp)
-            print("----------------------------------------", file=fp)
-            print("\x1b[0m", file=fp)
-            _describe_pycode(pycode, fp=fp, indent="")
-            print("\x1b[0m", file=fp)
-            print("----------------------------------------", file=fp)
-            print("", file=fp)
-            print("\x1b[32m\x1b[1merror:\x1b[0m", file=fp)
-            raise
+        for d in loader.load(stream, slurp=args.slurp_input):
+            try:
+                r = jqfpy.exec_pycode(d, pycode, rvalname)
+            except Exception as e:
+                fp = sys.stderr
+                print("\x1b[32m\x1b[1mcode:\x1b[0m", file=fp)
+                print("----------------------------------------", file=fp)
+                print("\x1b[0m", file=fp)
+                _describe_pycode(pycode, fp=fp, indent="")
+                print("\x1b[0m", file=fp)
+                print("----------------------------------------", file=fp)
+                print("", file=fp)
+                print("\x1b[32m\x1b[1merror:\x1b[0m", file=fp)
+                raise
 
-        dumper.dump(
-            r,
-            fp=sys.stdout,
-            squash=args.squash,
-            raw=args.raw_output,
-            json_kwargs=dict(
-                indent=None if args.compact_output else 2,
-                sort_keys=args.sort_keys,
-                ensure_ascii=args.ascii_output,
-            ),
-        )
+            dumper.dump(
+                r,
+                fp=fp,
+                squash=args.squash,
+                raw=args.raw_output,
+                json_kwargs=dict(
+                    indent=None if args.compact_output else 2,
+                    sort_keys=args.sort_keys,
+                    ensure_ascii=args.ascii_output,
+                ),
+            )
+            fp.flush()
 
 
 if __name__ == "__main__":
--- jqfpy/loader.py
+++ jqfpy/loader.py
@@ -3,25 +3,33 @@ from json.decoder import WHITESPACE
 
 
 def load(stream, *, slurp=False):
-    return loads(stream.read(), slurp=slurp)
+    if not slurp:
+        yield from loads(stream.read(), slurp=slurp)
+    else:
+        buf = []
+        for line in stream:
+            buf.append(line)
+            try:
+                d = json.loads("\n".join(buf))
+            except json.JSONDecodeError as e:
+                pass
+            else:
+                yield d
+                buf.clear()
 
 
 def loads(s, *, slurp=False):
-    if slurp:
-        return list(loads_slurp_iter(s))
+    if not slurp:
+        yield json.loads(s)
     else:
-        return json.loads(s)
+        size = len(s)
+        decoder = json.JSONDecoder()
 
-
-def loads_slurp_iter(s):
-    size = len(s)
-    decoder = json.JSONDecoder()
-
-    end = 0
-    while True:
-        idx = WHITESPACE.match(s[end:]).end()
-        i = end + idx
-        if i >= size:
-            break
-        ob, end = decoder.raw_decode(s, i)
-        yield ob
+        end = 0
+        while True:
+            idx = WHITESPACE.match(s[end:]).end()
+            i = end + idx
+            if i >= size:
+                break
+            ob, end = decoder.raw_decode(s, i)
+            yield ob

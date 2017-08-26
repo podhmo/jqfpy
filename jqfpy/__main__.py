0649c71f5c9b081bf087faf9718a7d6b909eaeb3
podhmo <ababjam61+github@gmail.com>
2017-08-26 17:29:55 +0900

    support -r. fixes #6

--- jqfpy/__main__.py
+++ jqfpy/__main__.py
@@ -13,20 +13,24 @@ def main():
     parser.add_argument("-s", "--slurp-input", action="store_true")
     parser.add_argument("-S", "--sort-keys", action="store_true")
     parser.add_argument("-a", "--ascii-output", action="store_true")
+    parser.add_argument("-r", "--raw-output", action="store_true")
     parser.add_argument("--squash", action="store_true")
 
     args = parser.parse_args()
 
     d = loader.load(args.input, slurp=args.slurp_input)
     r = transform(d, args.code)
-    dumper.dump(
-        r,
-        fp=sys.stdout,
-        squash=args.squash,
-        compact=args.compact_output,
-        sort_keys=args.sort_keys,
-        ensure_ascii=args.ascii_output,
-    )
+    if args.raw_output and isinstance(r, str):
+        print(r)
+    else:
+        dumper.dump(
+            r,
+            fp=sys.stdout,
+            squash=args.squash,
+            compact=args.compact_output,
+            sort_keys=args.sort_keys,
+            ensure_ascii=args.ascii_output,
+        )
 
 
 if __name__ == "__main__":

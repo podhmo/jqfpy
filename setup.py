f51b7015ef6decd5fac72d8287df6547e03e0468
podhmo <ababjam61+github@gmail.com>
2017-08-26 15:20:06 +0900

    init

---
+++ setup.py
@@ -0,0 +1,49 @@
+# -*- coding:utf-8 -*-
+
+import os
+import sys
+
+from setuptools import setup, find_packages
+here = os.path.abspath(os.path.dirname(__file__))
+try:
+    with open(os.path.join(here, 'README.rst')) as f:
+        README = f.read()
+    with open(os.path.join(here, 'CHANGES.txt')) as f:
+        CHANGES = f.read()
+except IOError:
+    README = CHANGES = ''
+
+install_requires = []
+
+docs_extras = []
+
+tests_require = []
+
+testing_extras = tests_require + []
+
+setup(
+    name='jqfpy',
+    version='0.0.0',
+    description='jq for pythonista',
+    long_description=README + '\n\n' + CHANGES,
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: Implementation :: CPython",
+    ],
+    keywords='jq',
+    author="podhmo",
+    author_email="ababjam61+github@gmail.com",
+    url="https://github.com/podhmo/jqfpy",
+    packages=find_packages(exclude=["jqfpy.tests"]),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=install_requires,
+    extras_require={
+        'testing': testing_extras,
+        'docs': docs_extras,
+    },
+    tests_require=tests_require,
+    test_suite="jqfpy.tests",
+    entry_points="""
+"""
+)

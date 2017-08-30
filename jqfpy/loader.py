4550d14cf24fb27d254006d0736d504fca72e70a
podhmo <ababjam61+github@gmail.com>
2017-08-31 04:21:16 +0900

    if broken json is passed, DecodeError is raised, correctly fixes #9

--- jqfpy/loader.py
+++ jqfpy/loader.py
@@ -4,12 +4,19 @@ from collections import deque
 
 def load(stream):
     buf = deque([], maxlen=100)
+    first_err = None
+
     for line in stream:
         buf.append(line)
         try:
             d = json.loads("\n".join(buf))
         except json.JSONDecodeError as e:
-            pass
+            if first_err is None:
+                first_err = e
         else:
+            first_err = None
             yield d
             buf.clear()
+
+    if first_err is not None:
+        raise first_err

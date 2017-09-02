fb94dbaac08d3ec4b84d9c0104f1bd491d997c61
podhmo <ababjam61+github@gmail.com>
2017-09-02 21:37:06 +0900

    example

--- examples/complicated/gen.py
+++ examples/complicated/gen.py
@@ -1,8 +1,10 @@
 import sys
 import json
+import time
 
 for i in range(5):
     print(json.dumps({"message": "stderr", "i": i}), file=sys.stderr)
     sys.stderr.flush()
     print(json.dumps({"message": "stdout", "i": i}))
     sys.stdout.flush()
+    time.sleep(0.05)

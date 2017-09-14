419127683619474a2c5ee6d4256010c317567aea
podhmo <ababjam61+github@gmail.com>
2017-09-15 08:56:07 +0900

    fix test

--- examples/complicated/gen.py
+++ examples/complicated/gen.py
@@ -3,8 +3,8 @@ import json
 import time
 
 for i in range(5):
-    print(json.dumps({"message": "stderr", "i": i}), file=sys.stderr)
+    print(json.dumps({"message": "stderr", "i": i}, sort_keys=True), file=sys.stderr)
     sys.stderr.flush()
-    print(json.dumps({"message": "stdout", "i": i}))
+    print(json.dumps({"message": "stdout", "i": i}, sort_keys=True))
     sys.stdout.flush()
     time.sleep(0.05)

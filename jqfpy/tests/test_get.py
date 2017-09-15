0414984f85fec0808cb5641b6e1c0706361393e5
podhmo <ababjam61+github@gmail.com>
2017-09-15 12:21:08 +0900

    add test

---
+++ jqfpy/tests/test_get.py
@@ -0,0 +1,22 @@
+import unittest
+
+
+class GetTests(unittest.TestCase):
+    def _makeOne(self, d):
+        from jqfpy import Getter
+        return Getter(d)
+
+    def test_rootdoc(self):
+        d = {"person": {"name": "foo", "age": 20}}
+        target = self._makeOne(d)
+
+        candidates = [
+            ("person", d["person"]),
+            ("person/name", "foo"),
+            ("person/age", 20),
+            (":missing", None),
+        ]
+        for k, expected in candidates:
+            with self.subTest(k=k):
+                got = target.get(k)
+                self.assertEqual(got, expected)

f2978ea4aff14ba352d3ebf3b89ecbb16adf2767
podhmo <ababjam61+github@gmail.com>
2019-04-10 15:57:16 +0900

    chore: dev extras

--- setup.py
+++ setup.py
@@ -1,40 +1,39 @@
 import os
 from setuptools import setup, find_packages
+
 here = os.path.abspath(os.path.dirname(__file__))
 try:
-    with open(os.path.join(here, 'README.rst')) as f:
+    with open(os.path.join(here, "README.rst")) as f:
         README = f.read()
-    with open(os.path.join(here, 'CHANGES.txt')) as f:
+    with open(os.path.join(here, "CHANGES.txt")) as f:
         CHANGES = f.read()
 except IOError:
-    README = CHANGES = ''
+    README = CHANGES = ""
 
 install_requires = ["magicalimport"]
 
 docs_extras = []
 
-yaml_extras = [
-    "PyYAML",
-]
+yaml_extras = ["PyYAML"]
 
 tests_require = []
 
 testing_extras = tests_require + []
 
 setup(
-    name='jqfpy',
-    version='0.4.3',
-    description='jq for pythonista',
-    long_description=README + '\n\n' + CHANGES,
+    name="jqfpy",
+    version="0.4.3",
+    description="jq for pythonista",
+    long_description=README + "\n\n" + CHANGES,
     classifiers=[
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy',
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
     ],
-    keywords='jq',
+    keywords="jq",
     author="podhmo",
     author_email="ababjam61+github@gmail.com",
     url="https://github.com/podhmo/jqfpy",
@@ -43,14 +42,15 @@ setup(
     zip_safe=False,
     install_requires=install_requires,
     extras_require={
-        'testing': testing_extras,
-        'yaml': yaml_extras,
-        'docs': docs_extras,
+        "testing": testing_extras,
+        "yaml": yaml_extras,
+        "docs": docs_extras,
+        "dev": ["black"],
     },
     tests_require=tests_require,
     test_suite="jqfpy.tests",
     entry_points="""
     [console_scripts]
     jqfpy=jqfpy.__main__:main
-"""
+""",
 )

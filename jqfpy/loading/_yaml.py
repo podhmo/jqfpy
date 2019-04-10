6752a9e23807dc6877f5516528735692c5ed3484
podhmo <ababjam61@gmail.com>
2019-04-10 15:53:03 +0900

    Include hash (#28)
    yaml output, Including hash

--- jqfpy/loading/_yaml.py
+++ jqfpy/loading/_yaml.py
@@ -1,5 +1,6 @@
-import yaml
+import re
 from collections import OrderedDict, defaultdict, ChainMap
+import yaml
 from .rec import consume_rec
 
 
@@ -13,18 +14,22 @@ class Loader(yaml.Loader):
 
 
 def _represent_odict(dumper, instance):
-    return dumper.represent_mapping('tag:yaml.org,2002:map', instance.items())
+    return dumper.represent_mapping("tag:yaml.org,2002:map", instance.items())
 
 
 def _construct_odict(loader, node):
     return OrderedDict(loader.construct_pairs(node))
 
 
-def _represent_str(dumper, instance):
+def _represent_str(dumper, instance, _rx=re.compile("[#:]")):
+    style = None
     if "\n" in instance:
-        return dumper.represent_scalar('tag:yaml.org,2002:str', instance, style='|')
+        style = "|"
     else:
-        return dumper.represent_scalar('tag:yaml.org,2002:str', instance)
+        m = _rx.search(instance)
+        if m is not None:
+            style = "'"
+    return dumper.represent_scalar("tag:yaml.org,2002:str", instance, style=style)
 
 
 def load(stream, *, buffered=False):
@@ -42,13 +47,13 @@ def dump(d, fp, *, squash_level=0, raw=False, extra_kwargs=None):
             fp,
             Dumper=Dumper,
             allow_unicode=allow_unicode,
-            default_flow_style=default_flow_style
+            default_flow_style=default_flow_style,
         )
 
     consume_rec(d, _dump, n=squash_level)
 
 
-Loader.add_constructor('tag:yaml.org,2002:map', _construct_odict)
+Loader.add_constructor("tag:yaml.org,2002:map", _construct_odict)
 for dict_class in [OrderedDict, defaultdict, ChainMap]:
     Dumper.add_representer(dict_class, _represent_odict)
 Dumper.add_representer(str, _represent_str)
